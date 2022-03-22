ea0f05437071ee6b3e646661918d70764df98974
LowlandVik <herboren@users.noreply.github.com>
2022-03-22 10:01:34 -0400

    Add files via upload

---
+++ ipglock.py
@@ -0,0 +1,15 @@
+import geoip2.database, json, os, re
+
+def ModelResponseToJSON(response):
+    pattern = "(\w.{1,}\()|(,\s\['\w+']\))"
+    response = re.sub(pattern, '', str(response))
+    return json.loads(response.replace("'",'"'))
+
+with geoip2.database.Reader(f'{os.getcwd()}\db\GeoLite2-City.mmdb') as reader:
+    response = ModelResponseToJSON(reader.city('')) 
+    latitude = response['location']['latitude']
+    longitude = response['location']['longitude']
+    print(f'Latitude: {latitude}\nLongitude: {longitude}')
+    
+
+
