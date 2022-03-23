822995cb710700c9a25057e2a8dee6bbf1bb95ce
herboren <herboren@users.noreply.github.com>
2022-03-22 21:12:04 -0400

    Updates

--- ipglock.py
+++ ipglock.py
@@ -1,17 +1,15 @@
-import geoip2.database, json, os, re
+import geoip2.database, json, os, re, simplekml, csv
+from ipaddress import ip_address, IPv4Address
 
-def ModelResponseToJSON(response):
-    pattern = "(\w.{1,}\()|(,\s\['\w+']\))"
-    response = re.sub(pattern, '', str(response))
-    return json.loads(response.replace("'",'"'))
+rowcount = 0
 
+# Open MaxMind db for reading
 with geoip2.database.Reader(f'{os.getcwd()}\db\GeoLite2-City.mmdb') as reader:
-    response = ModelResponseToJSON(reader.city('8.8.8.8')) 
-    print(response)
-    
-    #latitude = response['location']['latitude']
-    #longitude = response['location']['longitude']
-    #print(f'Latitude: {latitude}\nLongitude: {longitude}')
-    
-
-
+    # Open CSV file for IP table
+    with open('ru.csv', encoding='utf-8-sig') as csvd:
+            rows = csv.reader(csvd, dialect='excel', delimiter=',', quotechar="|")
+            # Get all IPs for validation
+            for row in rows:                
+                response = reader.city(row[0])
+                # Return GeoLocation
+                print(f"IP Address: {row[0]}\nCity: {response.city.name}\Lat/Lon: {response.location.latitude}, {response.location.longitude}")
