7fe72d8a5ab3a1e646fc09bf330ae5c423982195
herboren <herboren@users.noreply.github.com>
2022-03-22 21:32:09 -0400

    Update ipglock.py

--- ipglock.py
+++ ipglock.py
@@ -1,7 +1,9 @@
-import geoip2.database, json, os, re, simplekml, csv
+import geoip2.database, json, os, re, csv
 from ipaddress import ip_address, IPv4Address
 
-rowcount = 0
+uscenter = '207.250.97.159'
+# 38.2507,-85.7472
+# Louisville, KY
 
 # Open MaxMind db for reading
 with geoip2.database.Reader(f'{os.getcwd()}\db\GeoLite2-City.mmdb') as reader:
@@ -9,7 +11,10 @@ with geoip2.database.Reader(f'{os.getcwd()}\db\GeoLite2-City.mmdb') as reader:
     with open('ru.csv', encoding='utf-8-sig') as csvd:
             rows = csv.reader(csvd, dialect='excel', delimiter=',', quotechar="|")
             # Get all IPs for validation
-            for row in rows:                
-                response = reader.city(row[0])
-                # Return GeoLocation
-                print(f"IP Address: {row[0]}\nCity: {response.city.name}\Lat/Lon: {response.location.latitude}, {response.location.longitude}")
+            for row in rows:    
+                try:            
+                    response = reader.city(row[0])
+                    # Return GeoLocation
+                    print(f"IP Address: {row[0]}\n\nCity: {response.city.name}\nLat/Lon: {response.location.latitude}, {response.location.longitude}")
+                except Exception as ex:
+                    print()
